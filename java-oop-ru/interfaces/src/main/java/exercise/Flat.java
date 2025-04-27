437db845c4b9625c0b01f118f551c49408c481e0
@hexlet/cli <support@hexlet.io>
2025-04-27 20:56:35 +0300

    submit java-oop-ru/interfaces

---
+++ java-oop-ru/interfaces/src/main/java/exercise/Flat.java
@@ -0,0 +1,38 @@
+package exercise;
+
+// BEGIN
+public class Flat implements Home {
+    private final double area;
+    private final double balconyArea;
+    private final int floor;
+
+    public Flat(double area, int floor, double balconyArea) {
+        this.area = area;
+        this.floor = floor;
+        this.balconyArea = balconyArea;
+    }
+
+    @Override
+    public double getArea() {
+        return area + balconyArea;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Квартира площадью %.1f метров на %d этаже", getArea(), floor);
+    }
+
+    @Override
+    public int compareTo(Home another) {
+        double thisArea = this.getArea();
+        double anotherArea = another.getArea();
+
+        if (thisArea > anotherArea) {
+            return 1;
+        } else if (thisArea < anotherArea) {
+            return -1;
+        } else {
+            return 0;
+        }
+}
+// END
---
+++ java-oop-ru/interfaces/src/test/java/exercise/ReversedSequenceTest.java
@@ -0,0 +1,49 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ReversedSequenceTest {
+
+    @Test
+    void testToString() {
+        CharSequence text = new ReversedSequence("abcdef");
+        assertEquals("fedcba", text.toString());
+    }
+
+    @Test
+    void testCharAt() {
+        CharSequence text = new ReversedSequence("abcdef");
+        assertEquals('e', text.charAt(1));
+        assertEquals('a', text.charAt(5));
+    }
+
+    @Test
+    void testLength() {
+        CharSequence text = new ReversedSequence("abcdef");
+        assertEquals(6, text.length());
+
+        text = new ReversedSequence("");
+        assertEquals(0, text.length());
+    }
+
+    @Test
+    void testSubSequence() {
+        CharSequence text = new ReversedSequence("abcdef");
+        CharSequence sub = text.subSequence(1, 4);
+        assertEquals("edc", sub.toString());
+    }
+
+    @Test
+    void testEmptyString() {
+        CharSequence text = new ReversedSequence("");
+        assertEquals("", text.toString());
+        assertEquals(0, text.length());
+    }
+
+    @Test
+    void testSingleChar() {
+        CharSequence text = new ReversedSequence("a");
+        assertEquals("a", text.toString());
+        assertEquals('a', text.charAt(0));
+        assertEquals(1, text.length());
+    }
+}
